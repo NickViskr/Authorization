71eb0ffbb00cd84b22e4736c8d0dfed65c3cf676
NickViskr <nviskr@gmail.com>
2022-07-04 01:11:02 +0500

    Alert func

--- Authorization/ViewController.swift
+++ Authorization/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var userName: UITextField!
+    @IBOutlet var password: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
+    @IBAction func forgotUserNamePressed() {
+        showAlertName(with: "Your name", and: "Enter your name")
+        return
+    }
+    
+    @IBAction func forgotPasswordPressed() {
+        showAlertPassword(with: "Enter password", and: "enter the word Password")
+        return
+    }
+    
+}
 
+// MARK: - UIAlertController
+extension ViewController {
+     private func showAlertName(with title: String, and message: String) {
+        let alertForgotUN = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        present(alertForgotUN, animated: true)
+    }
+   
+    
+    private func showAlertPassword(with title: String, and message: String) {
+        let alertForgotPassword = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        present(alertForgotPassword, animated: true)
+    }
 }
 
